bb0697185af694817a0db181b94d80f0d9ee6c09
Victor Floyd <info@thekunductor.com>
2022-05-08 23:03:08 -0400

    added all fiiles

---
+++ ScaleKey.Me/script.js
@@ -0,0 +1,74 @@
+function one(){
+    document.getElementById('piano').src='images/piano_cmajoraminor.png';
+    document.getElementById('notebar').src='images/notebar_cmajoraminor.png';
+ }
+
+function two(){document.getElementById('piano').src='images/piano_gmajoreminor.png';
+               document.getElementById('notebar').src='images/notebar_gmajoreminor.png';
+            }
+
+function three(){
+     document.getElementById('piano').src='images/piano_dmajorbminor.png';
+     document.getElementById('notebar').src='images/notebar_dmajorbminor.png';
+}
+
+function four(){
+    document.getElementById('piano').src='images/piano_amajorfsharpminor.png';
+    document.getElementById('notebar').src='images/notebar_amajorfsharpminor.png';
+}
+
+function five(){
+    document.getElementById('piano').src='images/piano_emajorcsharpminor.png';
+    document.getElementById('notebar').src='images/notebar_emajorcsharpminor.png';
+}
+
+function six(){
+    document.getElementById('piano').src='images/piano_fmajordminor.png';
+    document.getElementById('notebar').src='images/notebar_fmajordminor.png';
+}
+
+function seven(){
+    document.getElementById('piano').src='images/piano_eflatmajorcminor.png';
+    document.getElementById('notebar').src='images/notebar_eflatmajorcminor.png';
+}
+
+function eight(){
+    document.getElementById('piano').src='images/piano_bflatmajorgminor.png';
+    document.getElementById('notebar').src='images/notebar_bflatmajorgminor.png';
+}
+
+function nine(){
+    document.getElementById('piano').src='images/piano_aflatmajorfminor.png';
+    document.getElementById('notebar').src='images/notebar_aflatmajorfminor.png';
+}
+
+function ten(){
+    document.getElementById('piano').src='images/piano_bmajorgsharpminor.png';
+    document.getElementById('notebar').src='images/notebar_bmajorgsharpminor.png';
+}
+
+function eleven(){
+    document.getElementById('piano').src='images/piano_cflatmajoraflatminor.png';
+    document.getElementById('notebar').src='images/notebar_cflatmajoraflatminor.png';
+}
+
+function twelve(){
+    document.getElementById('piano').src='images/piano_fsharpmajordsharpminor.png';
+    document.getElementById('notebar').src='images/notebar_fsharpmajordsharpminor.png';
+}
+
+function thirteen(){
+    document.getElementById('piano').src='images/piano_gflatmajoreflatminor.png';
+    document.getElementById('notebar').src='images/notebar_gflatmajoreflatminor.png';
+
+}
+
+function fourteen(){
+    document.getElementById('piano').src='images/piano_csharpmajorasharpminor.png';
+    document.getElementById('notebar').src='images/notebar_csharpmajorasharpminor.png';
+}
+
+function fifteen(){
+    document.getElementById('piano').src='images/piano_dflatmajorbflatminor.png';
+    document.getElementById('notebar').src='images/notebar_dflatmajorbflatminor.png';
+}
